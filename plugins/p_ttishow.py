84943757d3068240120a4443fb371470b7b670b1
Lastdrogz <105853088+Lastdrogz@users.noreply.github.com>
2022-06-01 13:36:53 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ 𝐇𝐞𝐥𝐩', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 𝐔𝐩𝐝𝐚𝐭𝐞𝐬', url='https://t.me/Happykidbgmz')
+            InlineKeyboardButton('📢 𝐔𝐩𝐝𝐚𝐭𝐞𝐬', url='https://t.me/KR_Botz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
